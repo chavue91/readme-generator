10766dc3af72690022cb21721071c89cbd2b995c
Cha Vue <chavue09@gmail.com>
2022-02-27 14:34:34 -0600

    Created an array of questions for user input

--- index.js
+++ index.js
@@ -4,7 +4,49 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.',
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select license for your application.',
+        choices: ['MIT', 'Apache', 'ISC', 'BSD', 'No license']
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide a link to the application github repo.',
+    },
+    // {
+    //     type: ,
+    //     name: ,
+    //     message: ,
+    // },
+    // {
+    //     type: ,
+    //     name: ,
+    //     message: ,
+    // },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
