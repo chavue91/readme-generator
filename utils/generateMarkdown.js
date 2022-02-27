f16f853a825fbe6701ae8544ca0c7e33798d6756
Cha Vue <chavue09@gmail.com>
2022-02-27 16:57:41 -0600

    removed license link from badge function and added link to licenseLink function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,13 +2,13 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license == 'MIT') {
-    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
   } else if (license == 'Apache') {
-    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
   } else if (license == 'ISC') {
-    return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
+    return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg))`
   } else if (license == 'BSD') {
-    return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    return `![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)`
   } else if (license == 'No license') {
     return ''
   }
@@ -16,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license == 'MIT') {
+    return `![License: MIT](https://opensource.org/licenses/MIT)`
+  } else if (license == 'Apache') {
+    return `![License](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license == 'ISC') {
+    return `![License: ISC](https://opensource.org/licenses/ISC)`
+  } else if (license == 'BSD') {
+    return `![License](https://opensource.org/licenses/BSD-3-Clause)`
+  } else if (license == 'No license') {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
